1b5601c0f1c4eec80babd3e89669e20b08219d5b
pb-bennett <pb.bennett.uk@gmail.com>
2022-02-11 23:21:11 +0100

    Had a couple of beers.  Time to commit before I break something ;)

--- script/script.js
+++ script/script.js
@@ -12,6 +12,12 @@ const rockBtnImgEl = document.getElementById("main-rock-img");
 const paperBtnImgEl = document.getElementById("main-paper-img");
 const scissorsBtnImgEl = document.getElementById("main-scissors-img");
 
+const battleModalEl = document.getElementById("battle-modal");
+const overlayEl = document.querySelector(".overlay");
+
+const commitImgEl = document.getElementById("commit-img");
+const commitBtnEl = document.getElementById("commit-btn");
+
 //defining colour variables
 const rockColor = "#ff36b5";
 const paperColor = "#45a2ff";
@@ -19,16 +25,32 @@ const scissorsColor = "#fff648";
 const mainColor = "#d1d1d1";
 
 //defining variables
+const options = ["rock", "paper", "scissors"];
+const scores = [0, 0];
+let chosenToggle = 0;
+let chosen;
 
-// const selector = ["rock", "paper", "scissors"];
+//defining click functions
 
-// console.log(selector.indexOf("paper"));
+const clickChoose = function (input) {
+  chosenToggle = 1;
+  chosen = input;
+  console.log(chosen);
+  for (let ch = 0; ch < 3; ch++) {
+    document.getElementById(
+      `logo-${options[ch]}-img`
+    ).src = `./image/${options[ch]}-title-grey.svg`;
+    document.getElementById(
+      `main-${options[ch]}-img`
+    ).src = `./image/${options[ch]}-grey.svg`;
+    document
+      .getElementById(`main-${options[ch]}-img`)
+      .classList.remove("shadow");
+  }
+  commitImgEl.src = "./image/commit-color.svg";
+  commitBtnEl.classList.add("shadow");
+  document.getElementById(`main-${input}-img`).classList.add("shadow");
 
-//defining mouse functions
-const mouseOverButton = function (input) {
-  rockLogoImg.src = "./image/rock-title-grey.svg";
-  paperLogoImg.src = "./image/paper-title-grey.svg";
-  scissorsLogoImg.src = "./image/scissors-title-grey.svg";
   document.getElementById(
     `logo-${input}-img`
   ).src = `./image/${input}-title-color.svg`;
@@ -36,34 +58,115 @@ const mouseOverButton = function (input) {
     `main-${input}-img`
   ).src = `./image/${input}-color.svg`;
 };
-const mouseOutButton = function (input) {
-  rockLogoImg.src = "./image/rock-title-color.svg";
-  paperLogoImg.src = "./image/paper-title-color.svg";
-  scissorsLogoImg.src = "./image/scissors-title-color.svg";
-  document.getElementById(
-    `main-${input}-img`
-  ).src = `./image/${input}-grey.svg`;
-  document.getElementById(`main-${input}-btn`).style = "Border: none;";
+
+//definging winning function
+
+const aWinner = function () {
+  if (scores[0] > scores[1]) {
+    console.log("You win!");
+  } else {
+    console.log("You lose!");
+  }
 };
 
-// Mouse interactions with main buttons - Rock
+//defining mouse functions
+const mouseOverButton = function (input) {
+  if (chosenToggle === 0) {
+    rockLogoImg.src = "./image/rock-title-grey.svg";
+    paperLogoImg.src = "./image/paper-title-grey.svg";
+    scissorsLogoImg.src = "./image/scissors-title-grey.svg";
+    document.getElementById(
+      `logo-${input}-img`
+    ).src = `./image/${input}-title-color.svg`;
+    document.getElementById(
+      `main-${input}-img`
+    ).src = `./image/${input}-color.svg`;
+  }
+};
+const mouseOutButton = function (input) {
+  if (chosenToggle === 0) {
+    rockLogoImg.src = "./image/rock-title-color.svg";
+    paperLogoImg.src = "./image/paper-title-color.svg";
+    scissorsLogoImg.src = "./image/scissors-title-color.svg";
+    document.getElementById(
+      `main-${input}-img`
+    ).src = `./image/${input}-grey.svg`;
+    document.getElementById(`main-${input}-btn`).style = "Border: none;";
+  }
+};
+const commitMove = function () {
+  if (scores[0] > 4 || scores[1] > 4) {
+    aWinner();
+  } else if (chosenToggle === 1) {
+    chosenToggle = 0;
+    const redChoice = Math.trunc(Math.random() * 3);
+    console.log("Red Choice:" + options[redChoice]);
+    console.log("click");
+    battleModalEl.classList.remove("hidden");
+    overlayEl.classList.remove("hidden");
+  }
+};
+
+// Mouse interactions with main and logo buttons - Rock
 rockBtnEl.addEventListener("mouseover", function () {
   mouseOverButton("rock");
 });
 rockBtnEl.addEventListener("mouseout", function () {
   mouseOutButton("rock");
 });
-// Mouse interactions with main buttons - Paper
+rockBtnEl.addEventListener("click", function () {
+  clickChoose("rock");
+});
+rockLogoImg.addEventListener("mouseover", function () {
+  mouseOverButton("rock");
+});
+rockLogoImg.addEventListener("mouseout", function () {
+  mouseOutButton("rock");
+});
+rockLogoImg.addEventListener("click", function () {
+  clickChoose("rock");
+});
+// Mouse interactions with main and logo buttons - Paper
 paperBtnEl.addEventListener("mouseover", function () {
   mouseOverButton("paper");
 });
 paperBtnEl.addEventListener("mouseout", function () {
   mouseOutButton("paper");
 });
-// Mouse interactions with main buttons - Scissors
+paperBtnEl.addEventListener("click", function () {
+  clickChoose("paper");
+});
+paperLogoImg.addEventListener("mouseover", function () {
+  mouseOverButton("paper");
+});
+paperLogoImg.addEventListener("mouseout", function () {
+  mouseOutButton("paper");
+});
+paperLogoImg.addEventListener("click", function () {
+  clickChoose("paper");
+});
+// Mouse interactions with main and logo buttons - Scissors
 scissorsBtnEl.addEventListener("mouseover", function () {
   mouseOverButton("scissors");
 });
 scissorsBtnEl.addEventListener("mouseout", function () {
   mouseOutButton("scissors");
+});
+scissorsBtnEl.addEventListener("click", function () {
+  clickChoose("scissors");
+});
+scissorsLogoImg.addEventListener("mouseover", function () {
+  mouseOverButton("scissors");
+});
+scissorsLogoImg.addEventListener("mouseout", function () {
+  mouseOutButton("scissors");
+});
+scissorsLogoImg.addEventListener("click", function () {
+  clickChoose("scissors");
+});
+
+// Mouse interactions wtih commit button
+
+commitBtnEl.addEventListener("click", function () {
+  commitMove();
 });
