868f868304a6ae977fd0216d801e51eb137d40f4
pb-bennett <pb.bennett.uk@gmail.com>
2022-02-11 19:18:32 +0100

    DOM almost laid out.  Now for more js

--- script/script.js
+++ script/script.js
@@ -4,6 +4,10 @@ const rockBtnEl = document.getElementById("main-rock-btn");
 const paperBtnEl = document.getElementById("main-paper-btn");
 const scissorsBtnEl = document.getElementById("main-scissors-btn");
 
+const rockLogoImg = document.getElementById("logo-rock-img");
+const paperLogoImg = document.getElementById("logo-paper-img");
+const scissorsLogoImg = document.getElementById("logo-scissors-img");
+
 const rockBtnImgEl = document.getElementById("main-rock-img");
 const paperBtnImgEl = document.getElementById("main-paper-img");
 const scissorsBtnImgEl = document.getElementById("main-scissors-img");
@@ -14,55 +18,52 @@ const paperColor = "#45a2ff";
 const scissorsColor = "#fff648";
 const mainColor = "#d1d1d1";
 
+//defining variables
+
+// const selector = ["rock", "paper", "scissors"];
+
+// console.log(selector.indexOf("paper"));
+
 //defining mouse functions
 const mouseOverButton = function (input) {
+  rockLogoImg.src = "./image/rock-title-grey.svg";
+  paperLogoImg.src = "./image/paper-title-grey.svg";
+  scissorsLogoImg.src = "./image/scissors-title-grey.svg";
+  document.getElementById(
+    `logo-${input}-img`
+  ).src = `./image/${input}-title-color.svg`;
   document.getElementById(
     `main-${input}-img`
   ).src = `./image/${input}-color.svg`;
-  // document.getElementById(`main-${input}-btn`).style =
-  // "box-shadow: rgba(255, 255, 255, 0.1) 0px 1px 1px 0px inset, rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px; border-top: 0.05rem solid #d1d1d1;border-left: 0.05rem solid #d1d1d1 ";
-  // document.getElementById(`main-${input}-btn`).style =
-  //   "Border: 3px solid #d1d1d1;"; Border: 0.15rem solid #d1d1d1;
-  // inputEl.src = `./image/${input}-raised.svg`;
 };
 const mouseOutButton = function (input) {
+  rockLogoImg.src = "./image/rock-title-color.svg";
+  paperLogoImg.src = "./image/paper-title-color.svg";
+  scissorsLogoImg.src = "./image/scissors-title-color.svg";
   document.getElementById(
     `main-${input}-img`
   ).src = `./image/${input}-grey.svg`;
   document.getElementById(`main-${input}-btn`).style = "Border: none;";
-  // inputEl.src = `./image/${input}-lowered.svg`;
-  // console.log(`mouse out ${input}`);
 };
 
 // Mouse interactions with main buttons - Rock
 rockBtnEl.addEventListener("mouseover", function () {
-  // rockBtnImgEl.src = "./image/rock-raised.svg";
-  // console.log("Click - Rock");
   mouseOverButton("rock");
 });
 rockBtnEl.addEventListener("mouseout", function () {
   mouseOutButton("rock");
-  // rockBtnImgEl.src = "./image/rock-lowered.svg";
-  // console.log("Click - Rock");
 });
 // Mouse interactions with main buttons - Paper
 paperBtnEl.addEventListener("mouseover", function () {
-  // paperBtnImgEl.src = "./image/paper-raised.svg";
-  // console.log("Click - Paper");
   mouseOverButton("paper");
 });
 paperBtnEl.addEventListener("mouseout", function () {
   mouseOutButton("paper");
-  // console.log("Click - Paper");
 });
 // Mouse interactions with main buttons - Scissors
 scissorsBtnEl.addEventListener("mouseover", function () {
-  // scissorsBtnImgEl.src = "./image/scissors-raised.svg";
-  // console.log("Click - Scissors");
   mouseOverButton("scissors");
 });
 scissorsBtnEl.addEventListener("mouseout", function () {
   mouseOutButton("scissors");
-  // scissorsBtnImgEl.src = "./image/scissors-lowered.svg";
-  // console.log("Click - Scissors");
 });
