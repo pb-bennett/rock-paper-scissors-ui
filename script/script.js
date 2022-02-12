196f88cb3271a2e16b0faa48d6f24ab821834803
pb-bennett <pb.bennett.uk@gmail.com>
2022-02-12 11:14:20 +0100

    Added game mechanics
    I need to get better at more often commiting - I can't remember everything I have done since the last one :(

--- script/script.js
+++ script/script.js
@@ -18,6 +18,12 @@ const overlayEl = document.querySelector(".overlay");
 const commitImgEl = document.getElementById("commit-img");
 const commitBtnEl = document.getElementById("commit-btn");
 
+const nextRoundBtnEl = document.getElementById("next-round-img");
+const roundResultEl = document.getElementById("round-result-img");
+
+const greenScoreImg = document.getElementById("green-score-img");
+const redScoreImg = document.getElementById("red-score-img");
+
 //defining colour variables
 const rockColor = "#ff36b5";
 const paperColor = "#45a2ff";
@@ -28,13 +34,13 @@ const mainColor = "#d1d1d1";
 const options = ["rock", "paper", "scissors"];
 const scores = [0, 0];
 let chosenToggle = 0;
-let chosen;
+const chosen = [0, 0];
 
 //defining click functions
 
 const clickChoose = function (input) {
   chosenToggle = 1;
-  chosen = input;
+  chosen[0] = input;
   console.log(chosen);
   for (let ch = 0; ch < 3; ch++) {
     document.getElementById(
@@ -59,6 +65,43 @@ const clickChoose = function (input) {
   ).src = `./image/${input}-color.svg`;
 };
 
+//defining round result function
+
+const roundResult = function (GM, RM) {
+  if (GM === RM) {
+    return "draw";
+  } else if (GM === "rock") {
+    if (RM === "paper") {
+      return "lose";
+    } else {
+      return "win";
+    }
+  } else if (GM === "paper") {
+    if (RM === "scissors") {
+      return "lose";
+    } else {
+      return "win";
+    }
+  } else {
+    if (RM === "rock") {
+      return "lose";
+    } else {
+      return "win";
+    }
+  }
+};
+
+//defining updating scores function
+
+const updateScores = function (res) {
+  if (res !== "draw") {
+    res === "win" ? scores[0]++ : scores[1]++;
+    greenScoreImg.src = `./image/score-green-${scores[0]}.svg`;
+    redScoreImg.src = `./image/score-red-${scores[1]}.svg`;
+  }
+  console.log(scores);
+};
+
 //definging winning function
 
 const aWinner = function () {
@@ -67,6 +110,33 @@ const aWinner = function () {
   } else {
     console.log("You lose!");
   }
+};
+
+//defining closing modal function
+
+const closeAll = function () {
+  battleModalEl.classList.add("hidden");
+  overlayEl.classList.add("hidden");
+  resetChosen();
+};
+
+//defining reset chosen function
+
+const resetChosen = function () {
+  chosenToggle = 0;
+  for (let ch = 0; ch < 3; ch++) {
+    document.getElementById(
+      `logo-${options[ch]}-img`
+    ).src = `./image/${options[ch]}-title-color.svg`;
+    document.getElementById(
+      `main-${options[ch]}-img`
+    ).src = `./image/${options[ch]}-grey.svg`;
+    document
+      .getElementById(`main-${options[ch]}-img`)
+      .classList.remove("shadow");
+  }
+  commitImgEl.src = "./image/commit-grey.svg";
+  commitImgEl.classList.remove("shadow");
 };
 
 //defining mouse functions
@@ -95,15 +165,27 @@ const mouseOutButton = function (input) {
   }
 };
 const commitMove = function () {
-  if (scores[0] > 4 || scores[1] > 4) {
-    aWinner();
-  } else if (chosenToggle === 1) {
+  if (chosenToggle === 1) {
     chosenToggle = 0;
-    const redChoice = Math.trunc(Math.random() * 3);
-    console.log("Red Choice:" + options[redChoice]);
+    chosen[1] = options[Math.trunc(Math.random() * 3)];
+    let result = roundResult(chosen[0], chosen[1]);
+    console.log("the reulst is:" + result);
+    roundResultEl.src = `./image/round-over-${result}.svg`;
+    updateScores(result);
+    console.log(chosen);
     console.log("click");
-    battleModalEl.classList.remove("hidden");
-    overlayEl.classList.remove("hidden");
+    if (scores[0] > 4 || scores[1] > 4) {
+      aWinner();
+    } else {
+      battleModalEl.classList.remove("hidden");
+      overlayEl.classList.remove("hidden");
+      for (let i = 0; i < 2; i++) {
+        document.getElementById(
+          `move-${i}-img`
+        ).src = `./image/${chosen[i]}-color.svg
+      `;
+      }
+    }
   }
 };
 
@@ -175,12 +257,22 @@ commitBtnEl.addEventListener("click", function () {
   commitMove();
 });
 
+nextRoundBtnEl.addEventListener("click", function () {
+  closeAll();
+});
+
 // escape key event listner to cancel and close
 
 document.addEventListener("keydown", function (e) {
+  console.log(e.key);
+  console.log(chosenToggle);
   if (e.key === "Escape") {
-    if (chosenToggle === 1) {
+    if (!battleModalEl.classList.contains("hidden")) {
       chosenToggle = 0;
+      closeAll();
+      console.log(e.key);
+    } else if (chosenToggle === 1) {
+      resetChosen();
     }
   }
 });
